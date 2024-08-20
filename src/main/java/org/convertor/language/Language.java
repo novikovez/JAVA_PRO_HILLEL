0b4fad922cc4310a7973c682558d7e01fff3efe9
Igor Novikov <novikov7ua@gmail.com>
2024-08-20 19:37:06 +0300

    Фаренгейти в цельсії

--- src/main/java/org/convertor/language/Language.java
+++ src/main/java/org/convertor/language/Language.java
@@ -11,6 +11,7 @@ public class Language {
     protected static final String SELECT_TYPE = "Виберіть тип конвертера";
     protected static final String SELECT_TYPE_1 = "1. Відстань";
     protected static final String SELECT_TYPE_2 = "2. Вага";
+    protected static final String SELECT_TYPE_3 = "3. Температура";
     protected static final String SELECT_POUNDS = "1. Фунти у кілограми";
     protected static final String SELECT_KGS = "2. Кілограми у фунти";
     protected static final String SELECT_MILES = "1. Милі у кілометри";
@@ -18,5 +19,7 @@ public class Language {
     protected static final String INVALID_INPUT = "Введіть числове значення! (Приклад 7 або 5.6)";
     protected static final String INPUT = "Введіть числове значення";
     protected static final String INVALID_SELECT = "Неприпустиме значення!";
+    protected static final String SELECT_T_FR_C = "1. Фаренгейти в цельсії";
+    protected static final String SELECT_T_C_FR = "2. Цельсії в фаренгейти";
 
 }
--- src/main/java/org/convertor/service/FactoryService.java
+++ src/main/java/org/convertor/service/FactoryService.java
@@ -6,6 +6,7 @@ package org.convertor.service;
 
 import org.convertor.language.Language;
 import org.convertor.service.distance.DistanceFactory;
+import org.convertor.service.temperature.TemperatureFactory;
 import org.convertor.service.weight.WeightFactory;
 
 import java.util.InputMismatchException;
@@ -19,10 +20,12 @@ public class FactoryService extends Language {
             System.out.println(SELECT_TYPE);
             System.out.println(SELECT_TYPE_1);
             System.out.println(SELECT_TYPE_2);
+            System.out.println(SELECT_TYPE_3);
             int input = scanner.nextInt();
             return switch (input) {
                 case 1 -> new DistanceFactory().getFactory();
                 case 2 -> new WeightFactory().getFactory();
+                case 3 -> new TemperatureFactory().getFactory();
                 default -> {
                     throw new InputMismatchException();
                 }
