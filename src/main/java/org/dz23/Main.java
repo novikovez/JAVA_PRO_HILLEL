19159176520dd5e4a830d8928afd84279e5964d9
Igor <macmini@Mac.lan>
2024-11-25 14:31:30 +0200

    DZ 23

--- src/main/java/org/dz23/Main.java
+++ src/main/java/org/dz23/Main.java
@@ -5,6 +5,11 @@ import org.password_generator.PasswordGenerator;
 public class Main {
     public static void main(String[] args) {
         String pass = PasswordGenerator.generatePassword(15);
-        System.out.println(pass);
+        System.out.println("Пароль: " + pass);
+        if(pass.length() == 15) {
+            System.out.println("Довжина збіглася");
+            return;
+        }
+        throw new IllegalArgumentException("Довжина не збіглася!");
     }
 }
